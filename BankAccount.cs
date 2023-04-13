3203eae6033e130d227dd37f8522278285be2cfd
m01h01 <mheang01@student.rtc.edu>
2023-04-12 21:32:23 -0700

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice___Constructors__Methods__and_Overloading
+{
+    internal class BankAccount
+    {
+        string _accounNumber;
+        string _name;
+        double _balance;
+
+        public BankAccount(string accounNumber, string name, double balance)
+        {
+            _accounNumber = accounNumber;
+            _name = name;
+            _balance = balance;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string AccounNumber { get => _accounNumber; }
+        public double Balance { get => _balance; }
+
+        public void Deposit(double amount)
+        {
+           if(amount > 0) 
+           {
+                _balance += amount;
+           }
+           
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > 0)
+            {
+                _balance -= amount;
+            }
+            else if (amount > _balance)
+            {
+                _balance = Balance;
+            }
+           
+        }
+        public override string ToString()
+        {
+            return $"{_accounNumber} - {_name} - Current Balance {_balance.ToString("C")}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Practice___Constructors__Methods__and_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car defaultCar= new Car();
+            Car makeModelCar = new Car("Toyota", "Rav4");
+            Car fullCar = new Car("Hyundai", "Sonata", 2010, 100000);
+
+            Console.WriteLine("Car Information\n" +
+                defaultCar + "\n"+
+                makeModelCar + "\n"+
+               fullCar + "\n\n");
+
+
+            Console.WriteLine("Bank Information:");
+            BankAccount account = new BankAccount("12345", "Will Cram", 20);
+            Console.WriteLine(account);
+
+            account.Deposit(500);
+            Console.WriteLine(account);
+
+            account.Deposit(-15000);
+            Console.WriteLine(account);
+
+            account.Withdraw(300);
+            Console.WriteLine(account);
+
+            account.Withdraw(-1);
+            Console.WriteLine(account);
+
+            account.Withdraw(-500);
+            Console.WriteLine(account);
+
+
+        }
+    }//class
+}//namespace
